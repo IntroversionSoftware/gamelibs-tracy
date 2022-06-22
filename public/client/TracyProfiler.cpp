e5fa52d3dae2afc21018008f9baa1f94cdaba604
Steven Noonan <steven@uplinklabs.net>
2022-06-21 17:20:56 -0700

    profiler: only use default "Main thread" name if no name was programatically specified
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- public/client/TracyProfiler.cpp
+++ public/client/TracyProfiler.cpp
@@ -3446,13 +3446,13 @@ bool Profiler::HandleServerQuery()
         SendString( ptr, (const char*)ptr, QueueType::StringData );
         break;
     case ServerQueryThreadString:
-        if( ptr == m_mainThread )
         {
-            SendString( ptr, "Main thread", 11, QueueType::ThreadName );
-        }
-        else
-        {
-            SendString( ptr, GetThreadName( ptr ), QueueType::ThreadName );
+            const char *threadName = GetThreadName(ptr);
+            if (ptr == m_mainThread && !HasThreadName(ptr)) {
+                SendString(ptr, "Main thread", 11, QueueType::ThreadName);
+            } else {
+                SendString(ptr, threadName, QueueType::ThreadName);
+            }
         }
         break;
     case ServerQuerySourceLocation:
