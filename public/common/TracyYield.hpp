bfcd4cfa76cd3438b21f66e70288126e78789d2b
Steven Noonan <steven@uplinklabs.net>
2022-06-22 10:35:47 -0700

    implement YieldThread for arm64 Windows
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- public/common/TracyYield.hpp
+++ public/common/TracyYield.hpp
@@ -1,8 +1,10 @@
 #ifndef __TRACYYIELD_HPP__
 #define __TRACYYIELD_HPP__
 
-#if defined __SSE2__ || defined _M_AMD64 || _M_IX86_FP == 2
+#if defined __SSE2__ || defined _M_AMD64 || (defined(_M_IX86) && _M_IX86_FP == 2)
 #  include <emmintrin.h>
+#elif defined(_MSC_VER) && (defined(_M_ARM) || defined(_M_ARM64))
+#  pragma intrinsic(__yield)
 #else
 #  include <thread>
 #endif
@@ -14,8 +16,10 @@ namespace tracy
 
 static tracy_force_inline void YieldThread()
 {
-#if defined __SSE2__ || defined _M_AMD64 || _M_IX86_FP == 2
+#if defined __SSE2__ || defined _M_AMD64 || (defined(_M_IX86) && _M_IX86_FP == 2)
     _mm_pause();
+#elif defined(_MSC_VER) && (defined(_M_ARM) || defined(_M_ARM64))
+    __yield();
 #elif defined __aarch64__
     asm volatile( "isb" : : );
 #else
