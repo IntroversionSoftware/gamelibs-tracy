06d159520fe9f254a38b17f70012452ce291c425
Steven Noonan <steven@uplinklabs.net>
2022-06-21 17:20:33 -0700

    callstacks: use SYMOPT_DEFERRED_LOADS for faster startup
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- public/client/TracyCallstack.cpp
+++ public/client/TracyCallstack.cpp
@@ -155,7 +155,7 @@ void InitCallstack()
 #endif
 
     SymInitialize( GetCurrentProcess(), nullptr, true );
-    SymSetOptions( SYMOPT_LOAD_LINES );
+    SymSetOptions( SYMOPT_LOAD_LINES | SYMOPT_DEFERRED_LOADS );
 
     DWORD needed;
     LPVOID dev[4096];
