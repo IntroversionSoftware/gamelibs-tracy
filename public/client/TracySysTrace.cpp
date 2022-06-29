f71fb610ba6b00f30222df8eaae62121edd0fc52
Steven Noonan <steven@uplinklabs.net>
2022-06-29 01:54:10 -0700

    timers: fix numerous timer bugs and implementation issues
    This is a complete rework of how timing is performed in Tracy. Instead
    of trying to use the TSC directly, just use the well-known good quality
    clocksource APIs, and wrap them in std::chrono interfaces.
    
    Bugs fixed:
    
     - Windows VSync and context switch time ranges work properly on non-x86
       now. The code assumed the event tracing timestamp was always
       represented in nanoseconds, but it was actually in 100ns NT timer
       ticks.
    
    Improvements:
    
     - Massive readability improvement for the clock source handling,
       instead of a deep proliferation of #ifdef combinations.
    
     - RDTSC is difficult to use because you need to measure the frequency
       of the clock source, and that is not always reliable. By using system
       clock sources, we allow the kernel to handle that job.
    
     - The QueryPerformanceCounter clock is now higher performance and
       trivial to use. One of the bottlenecks of using QPC/QPF is that you
       need to convert the tick counter value into nanoseconds via the
       queried frequency value. This involves a couple different integer
       divisions, which are very expensive.
    
       One thing the majority of users of these APIs are not aware of is
       that there are two extremely common QPF values: 10MHz and 24MHz. The
       former is very common on x86 systems, and the latter is very common
       on ARM64 devices. With that knowledge in mind, we can specialize the
       ticks-to-nanoseconds conversions when those frequencies are used.
       This avoids a series of integer divides if the compiler does a good
       job (from my testing, Clang, GCC, and MSVC all do great with this
       technique)
    
     - Added an ARM64-specific CPU clocksource. This clocksource, unlike the
       TSC, has a method to query its frequency. This makes it much more
       reliable than the TSC. This clocksource also frequently uses a 24MHz
       frequency, so we can do the ticks-to-nanoseconds specialization here,
       too.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- public/client/TracySysTrace.cpp
+++ public/client/TracySysTrace.cpp
@@ -102,7 +102,7 @@ void WINAPI EventRecordCallback( PEVENT_RECORD record )
             const auto cswitch = (const etw::CSwitch*)record->UserData;
 
             TracyLfqPrepare( QueueType::ContextSwitch );
-            MemWrite( &item->contextSwitch.time, hdr.TimeStamp.QuadPart );
+            MemWrite( &item->contextSwitch.time, hdr.TimeStamp.QuadPart * 100ULL );
             MemWrite( &item->contextSwitch.oldThread, cswitch->oldThreadId );
             MemWrite( &item->contextSwitch.newThread, cswitch->newThreadId );
             MemWrite( &item->contextSwitch.cpu, record->BufferContext.ProcessorNumber );
@@ -118,7 +118,7 @@ void WINAPI EventRecordCallback( PEVENT_RECORD record )
             const auto rt = (const etw::ReadyThread*)record->UserData;
 
             TracyLfqPrepare( QueueType::ThreadWakeup );
-            MemWrite( &item->threadWakeup.time, hdr.TimeStamp.QuadPart );
+            MemWrite( &item->threadWakeup.time, hdr.TimeStamp.QuadPart * 100ULL );
             MemWrite( &item->threadWakeup.cpu, record->BufferContext.ProcessorNumber );
             MemWrite( &item->threadWakeup.thread, rt->threadId );
             MemWrite( &item->threadWakeup.adjustReason, rt->adjustReason );
@@ -151,7 +151,7 @@ void WINAPI EventRecordCallback( PEVENT_RECORD record )
                     memcpy( trace, &sz, sizeof( uint64_t ) );
                     memcpy( trace+1, sw->stack, sizeof( uint64_t ) * sz );
                     TracyLfqPrepare( QueueType::CallstackSample );
-                    MemWrite( &item->callstackSampleFat.time, sw->eventTimeStamp );
+                    MemWrite( &item->callstackSampleFat.time, sw->eventTimeStamp * 100ULL );
                     MemWrite( &item->callstackSampleFat.thread, sw->stackThread );
                     MemWrite( &item->callstackSampleFat.ptr, (uint64_t)trace );
                     TracyLfqCommit;
@@ -164,7 +164,7 @@ void WINAPI EventRecordCallback( PEVENT_RECORD record )
         {
             const auto vs = (const etw::VSyncDPC*)record->UserData;
             TracyLfqPrepare( QueueType::FrameVsync );
-            MemWrite( &item->frameVsync.time, hdr.TimeStamp.QuadPart );
+            MemWrite( &item->frameVsync.time, hdr.TimeStamp.QuadPart * 100ULL);
             MemWrite( &item->frameVsync.id, vs->vidPnTargetId );
             TracyLfqCommit;
         }
@@ -608,15 +608,6 @@ bool SysTraceStart( int64_t& samplingPeriod )
     TracyDebug( "sched_waking id: %i", wakingId );
     TracyDebug( "drm_vblank_event id: %i", vsyncId );
 
-    bool useMonotonicClockRaw = !HardwareSupportsInvariantTSC();
-#if !defined TRACY_HW_TIMER || !defined TRACY_HAS_RDTSC
-    useMonotonicClockRaw = true;
-#endif
-    if( useMonotonicClockRaw )
-    {
-        TracyDebug( "Using CLOCK_MONOTONIC_RAW for Linux perf events." );
-    }
-
 #ifdef TRACY_NO_SAMPLING
     const bool noSoftwareSampling = true;
 #else
@@ -688,11 +679,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
     pe.disabled = 1;
     pe.freq = 1;
     pe.inherit = 1;
-    if( useMonotonicClockRaw )
-    {
-        pe.use_clockid = 1;
-        pe.clockid = CLOCK_MONOTONIC_RAW;
-    }
+    pe.use_clockid = 1;
+    pe.clockid = CLOCK_MONOTONIC_RAW;
 
     if( !noSoftwareSampling )
     {
@@ -734,11 +722,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
     pe.exclude_hv = 1;
     pe.freq = 1;
     pe.inherit = 1;
-    if( useMonotonicClockRaw )
-    {
-        pe.use_clockid = 1;
-        pe.clockid = CLOCK_MONOTONIC_RAW;
-    }
+    pe.use_clockid = 1;
+    pe.clockid = CLOCK_MONOTONIC_RAW;
 
     if( !noRetirement )
     {
@@ -861,11 +846,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
         pe.sample_type = PERF_SAMPLE_TIME | PERF_SAMPLE_RAW;
         pe.disabled = 1;
         pe.config = vsyncId;
-        if( useMonotonicClockRaw )
-        {
-            pe.use_clockid = 1;
-            pe.clockid = CLOCK_MONOTONIC_RAW;
-        }
+        pe.use_clockid = 1;
+        pe.clockid = CLOCK_MONOTONIC_RAW;
 
         TracyDebug( "Setup vsync capture" );
         for( int i=0; i<s_numCpus; i++ )
@@ -897,11 +879,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
         pe.disabled = 1;
         pe.inherit = 1;
         pe.config = switchId;
-        if( useMonotonicClockRaw )
-        {
-            pe.use_clockid = 1;
-            pe.clockid = CLOCK_MONOTONIC_RAW;
-        }
+        pe.use_clockid = 1;
+        pe.clockid = CLOCK_MONOTONIC_RAW;
 
         TracyDebug( "Setup context switch capture" );
         for( int i=0; i<s_numCpus; i++ )
@@ -931,11 +910,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
             pe.inherit = 1;
             pe.config = wakingId;
             pe.read_format = 0;
-            if( useMonotonicClockRaw )
-            {
-                pe.use_clockid = 1;
-                pe.clockid = CLOCK_MONOTONIC_RAW;
-            }
+            pe.use_clockid = 1;
+            pe.clockid = CLOCK_MONOTONIC_RAW;
 
             TracyDebug( "Setup waking up capture" );
             for( int i=0; i<s_numCpus; i++ )
@@ -1081,9 +1057,6 @@ void SysTraceWorker( void* ptr )
 
                         if( cnt > 0 )
                         {
-#if defined TRACY_HW_TIMER && defined TRACY_HAS_RDTSC
-                            t0 = ring.ConvertTimeToTsc( t0 );
-#endif
                             auto trace = GetCallstackBlock( cnt, ring, offset );
 
                             TracyLfqPrepare( QueueType::CallstackSample );
@@ -1115,9 +1088,6 @@ void SysTraceWorker( void* ptr )
                         offset += sizeof( uint64_t );
                         ring.Read( &t0, offset, sizeof( uint64_t ) );
 
-#if defined TRACY_HW_TIMER && defined TRACY_HAS_RDTSC
-                        t0 = ring.ConvertTimeToTsc( t0 );
-#endif
                         QueueType type;
                         switch( id )
                         {
@@ -1234,10 +1204,6 @@ void SysTraceWorker( void* ptr )
                         auto offset = rbPos;
                         perf_event_header hdr;
                         ring.Read( &hdr, offset, sizeof( perf_event_header ) );
-
-#if defined TRACY_HW_TIMER && defined TRACY_HAS_RDTSC
-                        t0 = ring.ConvertTimeToTsc( t0 );
-#endif
 
                         const auto rid = ring.GetId();
                         if( rid == EventContextSwitch )
