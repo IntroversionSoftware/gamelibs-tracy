c69cfd0478cc96efa95dbe378ad22fd112069417
Steven Noonan <steven@uplinklabs.net>
2022-06-29 01:54:10 -0700

    timers: fix numerous timer bugs and implementation issues
    This is a complete rework of how timing is performed in Tracy. Instead
    of trying to use the TSC directly, just use the well-known good quality
    clocksource APIs, and wrap them in std::chrono interfaces.
    
    Bugs fixed:
    
     - Windows VSync and context switch time ranges work properly on non-x86
       now. The code assumed the event tracing timestamp was always
       represented in nanoseconds, but it was actually in 100ns NT timer
       ticks.
    
    Improvements:
    
     - Massive readability improvement for the clock source handling,
       instead of a deep proliferation of #ifdef combinations.
    
     - RDTSC is difficult to use because you need to measure the frequency
       of the clock source, and that is not always reliable. By using system
       clock sources, we allow the kernel to handle that job.
    
     - The QueryPerformanceCounter clock is now higher performance and
       trivial to use. One of the bottlenecks of using QPC/QPF is that you
       need to convert the tick counter value into nanoseconds via the
       queried frequency value. This involves a couple different integer
       divisions, which are very expensive.
    
       One thing the majority of users of these APIs are not aware of is
       that there are two extremely common QPF values: 10MHz and 24MHz. The
       former is very common on x86 systems, and the latter is very common
       on ARM64 devices. With that knowledge in mind, we can specialize the
       ticks-to-nanoseconds conversions when those frequencies are used.
       This avoids a series of integer divides if the compiler does a good
       job (from my testing, Clang, GCC, and MSVC all do great with this
       technique)
    
     - Added an ARM64-specific CPU clocksource. This clocksource, unlike the
       TSC, has a method to query its frequency. This makes it much more
       reliable than the TSC. This clocksource also frequently uses a 24MHz
       frequency, so we can do the ticks-to-nanoseconds specialization here,
       too.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- public/client/TracySysTrace.cpp
+++ public/client/TracySysTrace.cpp
@@ -99,7 +99,7 @@ void WINAPI EventRecordCallback( PEVENT_RECORD record )
             const auto cswitch = (const etw::CSwitch*)record->UserData;
 
             TracyLfqPrepare( QueueType::ContextSwitch );
-            MemWrite( &item->contextSwitch.time, hdr.TimeStamp.QuadPart );
+            MemWrite( &item->contextSwitch.time, hdr.TimeStamp.QuadPart * 100ULL );
             MemWrite( &item->contextSwitch.oldThread, cswitch->oldThreadId );
             MemWrite( &item->contextSwitch.newThread, cswitch->newThreadId );
             MemWrite( &item->contextSwitch.cpu, record->BufferContext.ProcessorNumber );
@@ -115,7 +115,7 @@ void WINAPI EventRecordCallback( PEVENT_RECORD record )
             const auto rt = (const etw::ReadyThread*)record->UserData;
 
             TracyLfqPrepare( QueueType::ThreadWakeup );
-            MemWrite( &item->threadWakeup.time, hdr.TimeStamp.QuadPart );
+            MemWrite( &item->threadWakeup.time, hdr.TimeStamp.QuadPart * 100ULL );
             MemWrite( &item->threadWakeup.cpu, record->BufferContext.ProcessorNumber );
             MemWrite( &item->threadWakeup.thread, rt->threadId );
             MemWrite( &item->threadWakeup.adjustReason, rt->adjustReason );
@@ -148,7 +148,7 @@ void WINAPI EventRecordCallback( PEVENT_RECORD record )
                     memcpy( trace, &sz, sizeof( uint64_t ) );
                     memcpy( trace+1, sw->stack, sizeof( uint64_t ) * sz );
                     TracyLfqPrepare( QueueType::CallstackSample );
-                    MemWrite( &item->callstackSampleFat.time, sw->eventTimeStamp );
+                    MemWrite( &item->callstackSampleFat.time, sw->eventTimeStamp * 100ULL );
                     MemWrite( &item->callstackSampleFat.thread, sw->stackThread );
                     MemWrite( &item->callstackSampleFat.ptr, (uint64_t)trace );
                     TracyLfqCommit;
@@ -161,7 +161,7 @@ void WINAPI EventRecordCallback( PEVENT_RECORD record )
         {
             const auto vs = (const etw::VSyncDPC*)record->UserData;
             TracyLfqPrepare( QueueType::FrameVsync );
-            MemWrite( &item->frameVsync.time, hdr.TimeStamp.QuadPart );
+            MemWrite( &item->frameVsync.time, hdr.TimeStamp.QuadPart * 100ULL);
             MemWrite( &item->frameVsync.id, vs->vidPnTargetId );
             TracyLfqCommit;
         }
@@ -667,10 +667,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
     pe.disabled = 1;
     pe.freq = 1;
     pe.inherit = 1;
-#if !defined TRACY_HW_TIMER || !( defined __i386 || defined _M_IX86 || defined __x86_64__ || defined _M_X64 )
     pe.use_clockid = 1;
     pe.clockid = CLOCK_MONOTONIC_RAW;
-#endif
 
     if( !noSoftwareSampling )
     {
@@ -712,10 +710,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
     pe.exclude_hv = 1;
     pe.freq = 1;
     pe.inherit = 1;
-#if !defined TRACY_HW_TIMER || !( defined __i386 || defined _M_IX86 || defined __x86_64__ || defined _M_X64 )
     pe.use_clockid = 1;
     pe.clockid = CLOCK_MONOTONIC_RAW;
-#endif
 
     if( !noRetirement )
     {
@@ -838,10 +834,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
         pe.sample_type = PERF_SAMPLE_TIME | PERF_SAMPLE_RAW;
         pe.disabled = 1;
         pe.config = vsyncId;
-#if !defined TRACY_HW_TIMER || !( defined __i386 || defined _M_IX86 || defined __x86_64__ || defined _M_X64 )
         pe.use_clockid = 1;
         pe.clockid = CLOCK_MONOTONIC_RAW;
-#endif
 
         TracyDebug( "Setup vsync capture" );
         for( int i=0; i<s_numCpus; i++ )
@@ -873,10 +867,8 @@ bool SysTraceStart( int64_t& samplingPeriod )
         pe.disabled = 1;
         pe.inherit = 1;
         pe.config = switchId;
-#if !defined TRACY_HW_TIMER || !( defined __i386 || defined _M_IX86 || defined __x86_64__ || defined _M_X64 )
         pe.use_clockid = 1;
         pe.clockid = CLOCK_MONOTONIC_RAW;
-#endif
 
         TracyDebug( "Setup context switch capture" );
         for( int i=0; i<s_numCpus; i++ )
@@ -1055,9 +1047,6 @@ void SysTraceWorker( void* ptr )
 
                         if( cnt > 0 )
                         {
-#if defined TRACY_HW_TIMER && ( defined __i386 || defined _M_IX86 || defined __x86_64__ || defined _M_X64 )
-                            t0 = ring.ConvertTimeToTsc( t0 );
-#endif
                             auto trace = GetCallstackBlock( cnt, ring, offset );
 
                             TracyLfqPrepare( QueueType::CallstackSample );
@@ -1089,9 +1078,6 @@ void SysTraceWorker( void* ptr )
                         offset += sizeof( uint64_t );
                         ring.Read( &t0, offset, sizeof( uint64_t ) );
 
-#if defined TRACY_HW_TIMER && ( defined __i386 || defined _M_IX86 || defined __x86_64__ || defined _M_X64 )
-                        t0 = ring.ConvertTimeToTsc( t0 );
-#endif
                         QueueType type;
                         switch( id )
                         {
@@ -1208,10 +1194,6 @@ void SysTraceWorker( void* ptr )
                         auto offset = rbPos;
                         perf_event_header hdr;
                         ring.Read( &hdr, offset, sizeof( perf_event_header ) );
-
-#if defined TRACY_HW_TIMER && ( defined __i386 || defined _M_IX86 || defined __x86_64__ || defined _M_X64 )
-                        t0 = ring.ConvertTimeToTsc( t0 );
-#endif
 
                         const auto rid = ring.GetId();
                         if( rid == EventContextSwitch )
---
+++ public/client/TracyTimer.hpp
@@ -0,0 +1,199 @@
+#ifndef __TRACYTIMER_HPP__
+#define __TRACYTIMER_HPP__
+
+#include <chrono>
+#include <cstdint>
+#if defined _WIN32
+#include <intrin.h>
+#include <windows.h>
+#endif
+#if !defined _WIN32
+#include <time.h>
+#endif
+#ifdef __APPLE__
+#  include <TargetConditionals.h>
+#  include <mach/mach_time.h>
+#endif
+
+#include "../common/TracyForceInline.hpp"
+
+// TODO: Move these to some kind of common utility code location
+#if __cplusplus >= 201803L
+#define TRACY_LIKELY [[likely]]
+#define TRACY_UNLIKELY [[unlikely]]
+#else
+#define TRACY_LIKELY
+#define TRACY_UNLIKELY
+#endif
+
+#if defined _M_ARM || defined _M_ARM64 || defined __arm__ || defined __aarch64__
+#define TRACY_LIKELY_ARM TRACY_LIKELY
+#define TRACY_LIKELY_X86
+#elif defined _M_IX86 || defined _M_AMD64 || defined __i386__ || defined __x86_64__
+#define TRACY_LIKELY_ARM
+#define TRACY_LIKELY_X86 TRACY_LIKELY
+#else
+#define TRACY_LIKELY_ARM
+#define TRACY_LIKELY_X86
+#endif
+
+namespace tracy
+{
+namespace timers
+{
+namespace detail
+{
+
+static constexpr tracy_force_inline int64_t clock_scale_generic(int64_t _freq, int64_t _counter, int64_t _period_den)
+{
+    // Instead of just having "(_counter * _period_den) / _freq",
+    // the algorithm below prevents overflow when _counter is sufficiently large.
+    // It assumes that _freq * _period_den does not overflow, which is currently true for nano period.
+    // It is not realistic for _counter to accumulate to large values from zero with this assumption,
+    // but the initial value of _counter could be large.
+
+    const int64_t whole = (_counter / _freq) * _period_den;
+    const int64_t part = (_counter % _freq) * _period_den / _freq;
+    return whole + part;
+}
+
+// Specialization: if the frequency value is 10MHz, use this function. The
+// compiler recognizes the constants for frequency and time period and uses
+// shifts and multiplies instead of divides to calculate the nanosecond value.
+// This frequency is common on 64-bit x86 Windows.
+static constexpr tracy_no_inline int64_t clock_scale_10MHz(int64_t _counter, int64_t _period_den)
+{
+    constexpr int64_t _freq = 10000000LL;
+    return detail::clock_scale_generic(_freq, _counter, _period_den);
+}
+
+// Specialization: if the frequency value is 24MHz, use this function. The
+// compiler recognizes the constants for frequency and time period and uses
+// shifts and multiplies instead of divides to calculate the nanosecond value.
+// This frequency is common on ARM64 (Windows devices, and Apple Silicon Macs)
+static constexpr tracy_no_inline int64_t clock_scale_24MHz(int64_t _counter, int64_t _period_den)
+{
+    constexpr int64_t _freq = 24000000LL;
+    return detail::clock_scale_generic(_freq, _counter, _period_den);
+}
+
+#ifdef _WIN32
+static tracy_force_inline int64_t query_performance_frequency()
+{
+    LARGE_INTEGER freq;
+    QueryPerformanceFrequency(&freq);
+    return freq.QuadPart;
+}
+
+static tracy_force_inline int64_t query_performance_counter()
+{
+    LARGE_INTEGER ctr;
+    QueryPerformanceCounter(&ctr);
+    return ctr.QuadPart;
+}
+#endif
+
+} // namespace detail
+
+//
+// std::chrono compatible clock sources
+//
+// Various high-resolution clocksources, with more highly optimized code paths
+// than the C++ STL.
+//
+
+#if defined(CLOCK_MONOTONIC_RAW)
+struct monotonic_raw
+{
+    using duration    = std::chrono::nanoseconds;
+    using rep         = duration::rep;
+    using period      = duration::period;
+    using time_point  = std::chrono::time_point<monotonic_raw>;
+    static const bool is_steady = true;
+
+    static time_point now() noexcept
+    {
+        struct timespec ts;
+        clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+        std::chrono::duration<rep, period> time(std::chrono::seconds(ts.tv_sec) + std::chrono::nanoseconds(ts.tv_nsec));
+        return std::chrono::time_point<monotonic_raw>(time);
+    }
+};
+#endif
+
+#if defined(CLOCK_MONOTONIC)
+struct monotonic
+{
+    using duration    = std::chrono::nanoseconds;
+    using rep         = duration::rep;
+    using period      = duration::period;
+    using time_point  = std::chrono::time_point<monotonic_raw>;
+    static const bool is_steady = true;
+
+    static time_point now() noexcept
+    {
+        struct timespec ts;
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+        std::chrono::duration<rep, period> time(std::chrono::seconds(ts.tv_sec) + std::chrono::nanoseconds(ts.tv_nsec));
+        return std::chrono::time_point<monotonic_raw>(time);
+    }
+};
+#endif
+
+#if defined(_WIN32)
+struct win32_performance_counter
+{
+public:
+    using duration    = std::chrono::nanoseconds;
+    using rep         = duration::rep;
+    using period      = duration::period;
+    using time_point  = std::chrono::time_point<win32_performance_counter>;
+    static const bool is_steady = true;
+
+private:
+    static tracy_no_inline int64_t qpc_scale_dispatch(int64_t _freq, int64_t _counter)
+    {
+        switch (_freq) {
+        TRACY_LIKELY_X86 case 10000000LL: return detail::clock_scale_10MHz(_counter, period::den);
+        TRACY_LIKELY_ARM case 24000000LL: return detail::clock_scale_24MHz(_counter, period::den);
+        }
+        return detail::clock_scale_generic(_freq, _counter, period::den);
+    }
+
+public:
+    static time_point now() noexcept
+    {
+        static_assert(period::num == 1, "This assumes period::num == 1");
+        const int64_t freq = detail::query_performance_frequency();
+        const int64_t counter = detail::query_performance_counter();
+        return time_point(duration(qpc_scale_dispatch(freq, counter)));
+    }
+};
+#endif
+
+} // namespace timers
+
+#if defined(TRACY_TIMER_FALLBACK)
+
+using high_res_time = std::chrono::steady_clock;
+
+#else
+
+#if defined(CLOCK_MONOTONIC_RAW)
+using high_res_time = timers::monotonic_raw;
+#elif defined(CLOCK_MONOTONIC)
+using high_res_time = timers::monotonic;
+#elif defined(_WIN32)
+using high_res_time = timers::win32_performance_counter;
+#else
+using high_res_time = std::chrono::steady_clock;
+#endif
+
+#endif
+
+}
+
+#undef TRACY_LIKELY_X86
+#undef TRACY_LIKELY_ARM
+
+#endif
