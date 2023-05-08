5b4e5902319e5ed70811be0513da0b3354e7802e
Steven Noonan <steven@uplinklabs.net>
2023-05-08 04:30:54 -0700

    TracyQueue.hpp: add missing PlotFormatType::Watt
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- server/TracyWorker.cpp
+++ server/TracyWorker.cpp
@@ -5518,6 +5518,8 @@ void Worker::ProcessPlotConfig( const QueuePlotConfig& ev )
     } );
 
     plot->format = (PlotValueFormatting)ev.type;
+    if (plot->format == PlotValueFormatting::Watt)
+        plot->type = PlotType::Power;
     plot->showSteps = ev.step;
     plot->fill = ev.fill;
     plot->color = ev.color & 0xFFFFFF;
