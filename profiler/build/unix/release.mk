82393885212d3b8572a4761c503342cef428e73d
Steven Noonan <steven@uplinklabs.net>
2023-06-14 03:14:50 -0700

    profiler: build with -flto=thin on clang
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- profiler/build/unix/release.mk
+++ profiler/build/unix/release.mk
@@ -1,6 +1,10 @@
 CFLAGS := -O3
 ifndef TRACY_NO_LTO
+ifeq (1,$(shell $(CC) --version | grep clang > /dev/null && echo 1 || echo 0))
+CFLAGS += -flto=thin
+else
 CFLAGS += -flto
+endif
 endif
 DEFINES := -DNDEBUG
 BUILD := release
