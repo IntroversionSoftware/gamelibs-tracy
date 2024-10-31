5d7e69e764287c265b4712d64eca667476b603a5
Steven Noonan <steven@uplinklabs.net>
2024-10-31 03:28:45 -0700

    SafeCopyProlog: use ReadProcessMemory instead of SEH memcpy
    If SEH is not enabled, this would just fault anyway, so we should use
    the safer approach.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- public/client/TracyProfiler.cpp
+++ public/client/TracyProfiler.cpp
@@ -3038,14 +3038,7 @@ char* Profiler::SafeCopyProlog( const char* data, size_t size )
     if( size > SafeSendBufferSize ) buf = (char*)tracy_malloc( size );
 
 #ifdef _WIN32
-    __try
-    {
-        memcpy( buf, data, size );
-    }
-    __except( 1 /*EXCEPTION_EXECUTE_HANDLER*/ )
-    {
-        success = false;
-    }
+	success = ReadProcessMemory( GetCurrentProcess(), data, buf, size, nullptr ) != 0;
 #else
     // Send through the pipe to ensure safe reads
     for( size_t offset = 0; offset != size; /*in loop*/ )
