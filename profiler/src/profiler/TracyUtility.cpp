2727db7fe672dd8052fedef57c9b83fcd70b5008
Steven Noonan <steven@uplinklabs.net>
2025-11-15 11:13:45 -0800

    tracyview: fix buffer overflow with very large item name
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- profiler/src/profiler/TracyUtility.cpp
+++ profiler/src/profiler/TracyUtility.cpp
@@ -213,4 +213,26 @@ std::vector<std::string> SplitLines( const char* data, size_t sz )
     return ret;
 }
 
+size_t strlcpy(char *dst, const char *src, size_t dstSize)
+{
+	char *d = dst;
+	const char *s = src;
+	size_t n = dstSize;
+	/* Copy as many bytes as will fit */
+	if (n != 0) {
+		while (--n != 0) {
+			if ((*d++ = *s++) == '\0')
+				break;
+		}
+	}
+	/* Not enough room in dst, add NUL and traverse rest of src */
+	if (n == 0) {
+		if (dstSize != 0)
+			*d = '\0'; /* NUL-terminate dst */
+		while (*s++)
+			;
+	}
+	return (s - src - 1); /* count does not include NUL */
+}
+
 }
